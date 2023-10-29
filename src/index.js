e1e480d0163bcba2227fea59960e80163b2bac20
Grigori Konopelko <grigori.konopelko@gmail.com>
2023-10-30 00:47:49 +0300

    uuh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketPairs = { '[':']', '{':'}', '(':')' };                              
+  const closingBrackets = new Set(Object.values(bracketPairs)); 
+  function bracketsAreBalanced(str) {                                            
+    const open = [] // stack of (closing) brackets that need to be closed                       
+    for (char of str) {                                                        
+        if (closingBrackets.has(char)) {                                        
+            if (char === open[open.length-1]) open.pop()                        
+            else return false;                                                  
+        }                                                                       
+        if (char in bracketPairs) open.push(bracketPairs[char])                 
+    }                                                                           
+    return open.length === 0                                                    
+}    
+if (bracketsAreBalanced(str) !== bracketsConfig) {
+  return true;
+};
+return false;
+}       
